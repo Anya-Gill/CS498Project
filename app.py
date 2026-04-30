61eeac1faeb2869623cc5caf620dee2ef3d38d93
Hasan Al Saeedi <102138625+Hassoonu@users.noreply.github.com>
2026-04-29 19:20:55 -0500

    Added input from client using url
    changed code to take in hashtag input from the url, rather than running request.args.get, to continue the same format used by the client when they interact with their flask server

--- app.py
+++ app.py
@@ -83,9 +83,9 @@ def most_active_users():
 
 
 # ── Query 3 – Tweets by hashtag ───────────────────────────────────────────────
-@app.route("/tweets-by-hashtag")
-def tweets_by_hashtag():
-    hashtag = request.args.get("hashtag")
+@app.route("/tweets-by-hashtag/<hashtag>")
+def tweets_by_hashtag(hashtag):
+    # hashtag = request.args.get("hashtag")
 
     results = list(col.find(
         {"entities.hashtags.text": hashtag},
